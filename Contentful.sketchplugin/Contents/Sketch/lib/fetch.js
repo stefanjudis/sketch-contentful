0ed19bc3c1178d0e167c787827b2a05dfd623e2d
stefan judis <stefanjudis@gmail.com>
2017-01-25 17:33:45 +0100

    fix api call

--- Contentful.sketchplugin/Contents/Sketch/lib/fetch.js
+++ Contentful.sketchplugin/Contents/Sketch/lib/fetch.js
@@ -6,26 +6,43 @@
  */
 function populateData( context ) {
   initContext( context );
-
   var counts = {
     successful : 0,
     failed     : 0
   };
 
+  var spaceId = command.valueForKey_onDocument_forPluginIdentifier(
+    'spaceId',
+    docData,
+    'contentful'
+  );
+
+  var cdaToken = command.valueForKey_onDocument_forPluginIdentifier(
+    'cdaToken',
+    docData,
+    'contentful'
+  );
+
+  if ( spaceId.trim() == '' || cdaToken.trim() == '' ) {
+    log( 'in it' );
+    return context.api().message(
+      'Please define Space Id and Content Delivery API token in the settings'
+    );
+  }
+
   var fieldLayers = page.children().forEach( function( kid ) {
     var connectedEntry = kid.name().match( /{entry:(.+?)}/ );
 
     if ( connectedEntry ) {
-      var data = fetchJSON( 'https://cdn.contentful.com/spaces/f20lfrunubsq/entries?access_token=0f72dc3f46a2fd76a80145a476ef0f9daf5adce19462787499c1b0f4f2d1c98e&sys.id=' + connectedEntry[ 1 ] ).items[ 0 ];
-
-      log( data );
+      var data = fetchJSON(
+        'https://cdn.contentful.com/spaces/' + spaceId + '/entries?access_token=' + cdaToken + '&sys.id=' + connectedEntry[ 1 ]
+      ).items[ 0 ];
 
       kid.children().forEach( function( layer ) {
         var connectedField = layer.name().match( /{fields:(.+?)}/ );
 
         if ( connectedField ) {
           if ( /^<MSTextLayer:/.test( layer.toString() ) ) {
-            log( connectedField[ 1 ] );
             if ( data.fields[ connectedField[ 1 ] ] ) {
               layer.setStringValue( data.fields[ connectedField[ 1 ] ] );
 
