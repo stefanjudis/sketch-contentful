d499520d8867ee306053494fac8fc6283fcdbd2b
stefan judis <stefanjudis@gmail.com>
2017-01-31 22:44:06 +0100

    go for 2 spaces everywhere

--- Contentful.sketchplugin/Contents/Sketch/lib/utils.js
+++ Contentful.sketchplugin/Contents/Sketch/lib/utils.js
@@ -13,11 +13,11 @@ var page;
 var artboard;
 
 function initContext(context) {
-    doc = context.document;
-    docData = context.document.documentData();
-    plugin = context.plugin;
-    command = context.command;
-    page = doc.currentPage();
+  doc = context.document;
+  docData = context.document.documentData();
+  plugin = context.plugin;
+  command = context.command;
+  page = doc.currentPage();
 }
 
 //--------------------------------------//
@@ -25,95 +25,95 @@ function initContext(context) {
 //--------------------------------------//
 
 var getSize = {
-    width : function(layer) {
-      return layer.frame().width()
-    },
-    widthProportion : function(layer) {
-      return layer.frame().width()/layer.parentGroup().frame().width()
-    },
-    height : function(layer) {
-      return layer.frame().height()
-    },
-    heightProportion : function(layer) {
-      return layer.frame().height()/layer.parentGroup().frame().height()
-    },
-}
+  width : function( layer ) {
+    return layer.frame().width()
+  },
+  widthProportion : function( layer ) {
+    return layer.frame().width()/layer.parentGroup().frame().width()
+  },
+  height : function( layer ) {
+    return layer.frame().height()
+  },
+  heightProportion : function( layer ) {
+    return layer.frame().height()/layer.parentGroup().frame().height()
+  }
+};
 
 
 //--------------------------------------//
 //               Cocoa UI               //
 //--------------------------------------//
-function createLabel(text, fontSize, bold, frame) {
-    var label = NSTextField.alloc().initWithFrame(frame)
-    label.setStringValue(text)
-    label.setFont((bold) ? NSFont.boldSystemFontOfSize(fontSize) : NSFont.systemFontOfSize(fontSize))
-    label.setBezeled(false)
-    label.setDrawsBackground(false)
-    label.setEditable(false)
-    label.setSelectable(false)
+function createLabel( text, fontSize, bold, frame ) {
+  var label = NSTextField.alloc().initWithFrame( frame );
+  label.setStringValue( text );
+  label.setFont( ( bold ) ? NSFont.boldSystemFontOfSize( fontSize ) : NSFont.systemFontOfSize( fontSize ) );
+  label.setBezeled( false );
+  label.setDrawsBackground( false );
+  label.setEditable( false );
+  label.setSelectable( false );
 
-    return label
+  return label;
 }
 
 function createLogBox(errors, fontSize, frame) {
-    var logContainer = NSTextView.alloc().initWithFrame( frame );
-    var errorMsgs = errors.join( '\n\n' );
+  var logContainer = NSTextView.alloc().initWithFrame( frame );
+  var errorMsgs = errors.join( '\n\n' );
 
-    logContainer.setEditable(true);
-    logContainer.setSelectable(true);
-    logContainer.string = errorMsgs;
+  logContainer.setEditable(true);
+  logContainer.setSelectable(true);
+  logContainer.string = errorMsgs;
 
-    return logContainer;
+  return logContainer;
 }
 
 
 function createSettingsWindow( values ) {
-    var alert = COSAlertWindow.new()
-    var width = 400;
-    var freeSpace = width - 100;
+  var alert = COSAlertWindow.new();
+  var width = 400;
+  var freeSpace = width - 100;
 
-    alert.addButtonWithTitle( 'Save' );
-    alert.addButtonWithTitle( 'Cancel' );
-    alert.setMessageText( 'Sketch Contentful' );
-    alert.setInformativeText( 'Set your Space Id and Content Delivery Token to pull real production data into your designs.' );
-    alert.setIcon( NSImage.alloc().initByReferencingFile( plugin.urlForResourceNamed( 'icon@2x.png' ).path() ) );
+  alert.addButtonWithTitle( 'Save' );
+  alert.addButtonWithTitle( 'Cancel' );
+  alert.setMessageText( 'Sketch Contentful' );
+  alert.setInformativeText( 'Set your Space Id and Content Delivery Token to pull real production data into your designs.' );
+  alert.setIcon( NSImage.alloc().initByReferencingFile( plugin.urlForResourceNamed( 'icon@2x.png' ).path() ) );
 
-    var mainView = NSView.alloc().initWithFrame( NSMakeRect( 0, 0, width, 150 ) );
-    var spaceIdLabel = createLabel( 'Space ID', 12, true, NSMakeRect( 0, 120, freeSpace, 20 ) );
-    var spaceIdTextfield = NSTextField.alloc().initWithFrame( NSMakeRect( 0, 95, freeSpace, 25 ) );
-    spaceIdTextfield.setStringValue( values.spaceId || '' );
+  var mainView = NSView.alloc().initWithFrame( NSMakeRect( 0, 0, width, 150 ) );
+  var spaceIdLabel = createLabel( 'Space ID', 12, true, NSMakeRect( 0, 120, freeSpace, 20 ) );
+  var spaceIdTextfield = NSTextField.alloc().initWithFrame( NSMakeRect( 0, 95, freeSpace, 25 ) );
+  spaceIdTextfield.setStringValue( values.spaceId || '' );
 
-    var cdaTokenLabel = createLabel( 'Content Delivery API Token', 12, true, NSMakeRect( 0, 60, freeSpace, 20 ) );
-    var cdaTokenTextfield = NSTextField.alloc().initWithFrame( NSMakeRect( 0, 35, freeSpace, 25 ) );
-    cdaTokenTextfield.setStringValue( values.cdaToken || '' );
+  var cdaTokenLabel = createLabel( 'Content Delivery API Token', 12, true, NSMakeRect( 0, 60, freeSpace, 20 ) );
+  var cdaTokenTextfield = NSTextField.alloc().initWithFrame( NSMakeRect( 0, 35, freeSpace, 25 ) );
+  cdaTokenTextfield.setStringValue( values.cdaToken || '' );
 
-    mainView.addSubview( spaceIdLabel );
-    mainView.addSubview( spaceIdTextfield );
+  mainView.addSubview( spaceIdLabel );
+  mainView.addSubview( spaceIdTextfield );
 
-    mainView.addSubview( cdaTokenLabel );
-    mainView.addSubview( cdaTokenTextfield );
+  mainView.addSubview( cdaTokenLabel );
+  mainView.addSubview( cdaTokenTextfield );
 
-    alert.addAccessoryView( mainView );
+  alert.addAccessoryView( mainView );
 
-    var inputs = [ spaceIdTextfield, cdaTokenTextfield ];
-    return [ alert, inputs ];
+  var inputs = [ spaceIdTextfield, cdaTokenTextfield ];
+  return [ alert, inputs ];
 }
 
 function createErrorReportWindow( errors ) {
-    var report = COSAlertWindow.new()
-    var width = 400;
-    var freeSpace = width - 100;
+  var report = COSAlertWindow.new();
+  var width = 400;
+  var freeSpace = width - 100;
 
-    report.addButtonWithTitle( 'Ok' );
-    report.setMessageText( 'There have been errors' );
-    report.setInformativeText( 'Please check the error log to see what went wrong:' );
-    report.setIcon( NSImage.alloc().initByReferencingFile( plugin.urlForResourceNamed( 'icon@2x.png' ).path() ) );
+  report.addButtonWithTitle( 'Ok' );
+  report.setMessageText( 'There have been errors' );
+  report.setInformativeText( 'Please check the error log to see what went wrong:' );
+  report.setIcon( NSImage.alloc().initByReferencingFile( plugin.urlForResourceNamed( 'icon@2x.png' ).path() ) );
 
-    var mainView = NSView.alloc().initWithFrame( NSMakeRect( 0, 0, width, 250 ) );
-    var logBox = createLogBox( errors, 12, NSMakeRect( 0, 0, freeSpace, 250 ) );
+  var mainView = NSView.alloc().initWithFrame( NSMakeRect( 0, 0, width, 250 ) );
+  var logBox = createLogBox( errors, 12, NSMakeRect( 0, 0, freeSpace, 250 ) );
 
-    mainView.addSubview( logBox );
-    report.addAccessoryView( mainView );
+  mainView.addSubview( logBox );
+  report.addAccessoryView( mainView );
 
-    return report;
+  return report;
 }
